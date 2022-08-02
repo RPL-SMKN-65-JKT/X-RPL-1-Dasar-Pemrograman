08de9554a4caa13a2c3e8d25bbb33ca9cd1e0681
juandenies <109999707+juandenies@users.noreply.github.com>
2022-08-02 16:55:18 +0700

    Add files via upload

---
+++ VOLUME BALOK.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int p;
+    int l;
+    int t;
+    int hasil;
+
+    cout <<"Masukan Panjang: ";
+    cin >> p;
+    cout << "Maukan Luas: ";
+    cin >> l;
+    cout << "Masukan Tinggi: ";
+    cin >> t;
+    hasil = p*l*t;
+    cout << "Hasilnya adalah: " << hasil;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA TERGANTENG \n\n");
+printf("Nama       :Aldy Ana Juandenies\n");
+printf("Kelas      :X-RPL1\n");
+printf("Umur       :16\n");
+printf("Hobi       :Berenang\n");
+return 0;
+ }
