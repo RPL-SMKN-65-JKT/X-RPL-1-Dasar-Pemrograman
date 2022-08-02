db21437160cb0a8530b058a6c7fb3e0362f9edb3
KAL-EL25 <109999763+KAL-EL25@users.noreply.github.com>
2022-08-02 09:49:51 +0700

    Add files via upload

---
+++ AZREIL_16/biodatasiswa.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA\n\n");
+printf("Nama       :Muhammad Azreil Ramadhan\n");
+printf("Kelas      :X-RPL1\n");
+printf("Absen      :16\n");
+printf("Umur       :15\n");
+printf("Hobby      :Nonton Film\n");
+return 0;
+
+}
---
+++ AZREIL_16/volumebalok.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int p,l,t;
+int hasil;
+
+cout << "Type P:  ";
+cin >> p;
+cout << "Type L:  ";
+cin >> l;
+cout << "Type T:  ";
+cin >> t;
+hasil = p*l*t;
+cout << "Hasilnya adalah: "  << hasil;
+return 0;
+}
