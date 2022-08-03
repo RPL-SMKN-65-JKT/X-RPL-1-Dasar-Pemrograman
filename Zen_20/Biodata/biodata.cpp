aad3522e1caf2b9db6f1f0f017201f1a1bdaea5a
athnf <70727135+athnf@users.noreply.github.com>
2022-08-03 19:07:24 +0700

    Add files via upload

---
+++ Zen_20/Biodata/biodata.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA COOL \n\n");
+printf("Nama        Muhammad Zen Alhadi\n");
+printf("Kelas      :X-RPL1\n");
+printf("Umur       :16\n");
+printf("Hobi       : memancing\n");
+return 0;
+ }
---
+++ Zen_20/VolumeBalok/volume balok.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int p,l,t,v; // Deklarasi variabel panjang,lebar,tinggi,volume
+    string dimensi;
+
+    cout<<"PROGRAM C++ MENGHTUNG VOLUME BALOK"<<endl;
+    cout<<"----------------------------------"<<endl;
+    //Menginput nilai panjang,lebar dan tinggi
+    cout<<"Masukan Panjang : "; 
+    cin>>p;
+    cout<<"Masukan Lebar   : ";
+    cin>>l;
+    cout<<"Masukan Tinggi  : ";
+    cin>>t;
+    cout<<"Masukan Satuan (mm,cm,m) Dimensi Balok : ";
+    cin>>dimensi;
+    cout<<endl;
+    
+    v=p*l*t; //Rumus untuk menghitung volume balok
+    
+    cout<<"Volume Balok    = "<<v<<" "<<dimensi<<"3"; //Tampilkan hasil
+    return 0;
+
+}
