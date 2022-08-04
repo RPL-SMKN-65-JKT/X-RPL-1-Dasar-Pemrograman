538cf9dccde81592e135e1e04faeb03de9edef5c
mukhttar123 <109999504+mukhttar123@users.noreply.github.com>
2022-08-04 09:26:51 +0700

    Add files via upload

---
+++ Fathir_10/biodata.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA COOL \n\n");
+printf("Nama       :Fatir Ardhani\n");
+printf("Kelas      :X-RPL1\n");
+printf("Umur       :15\n");
+printf("Hobi       :Bermain Musik\n");
+return 0;
+ }
