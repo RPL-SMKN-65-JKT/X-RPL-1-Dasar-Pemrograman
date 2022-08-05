0e171d51aae9a346b37243aeab838e220e5db0a2
OdangzKetjeh <109999633+OdangzKetjeh@users.noreply.github.com>
2022-08-05 07:35:54 +0700

    Add files via upload

---
+++ Ahmad Fauzan_3/Volume Balok.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int p;
+int l;
+int t;
+int hasil;
+
+cout << "Type p: \n";
+cin >> p;
+cout << "Type l: \n";
+cin >> l;
+cout << "Type t: \n";
+cin >> t;
+hasil = p*l*t;
+cout <<"Hasilnya Adalah" << hasil;
+return 0;
+}
---
+++ Ahmad Fauzan_3/biodata.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA COOL\n\n");
+printf("Nama       :Ahmad Fauzan Misbakhul Anam\n");
+printf("Kelas      :X-RPL1\n");
+printf("Umur       :15\n");
+printf("Hobi       :Belajar Program\n");
+return 0;
+ }
