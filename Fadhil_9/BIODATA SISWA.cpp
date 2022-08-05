ebc981cf59e0fc4e89d19158ba8e94726be1ce44
OdangzKetjeh <109999633+OdangzKetjeh@users.noreply.github.com>
2022-08-05 07:34:58 +0700

    Add files via upload

---
+++ Fadhil_9/BIODATA SISWA.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ system("color a");
+ printf("\tBIODATA SISWA\n\n");
+ printf("Nama    :Fadhil Hayang Layana\n");
+ printf("Kelas   :X-RPL1\n");
+ printf("Absen   :9\n");
+ printf("Umur    :15\n");
+ printf("Hobby  :Futsal");
+ return 0;
+ }
---
+++ Fadhil_9/VOLUME BALOK.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int p;
+    int l;
+    int t;
+    int hasil;
+    cout << "masukan panjang: ";
+    cin >> p;
+    cout << "masukan lebar: ";
+    cin >> l;
+    cout << "masukan tinggi: ";
+    cin >> t;
+    hasil = p*l*t;
+    cout << "hasilnya adalah: " << hasil;
+    return 0;
+}
