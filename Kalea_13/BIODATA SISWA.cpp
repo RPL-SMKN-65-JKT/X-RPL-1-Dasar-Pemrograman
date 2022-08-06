cfa4c14a43b4e0ce7064699dba10dc5ae1272152
kaleaaa13 <109999653+kaleaaa13@users.noreply.github.com>
2022-08-06 15:05:47 +0700

    Add files via upload

---
+++ Kalea_13/BIODATA SISWA.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA COOL \n\n");
+printf("Nama       :Kalea Idzni Aulia\n");
+printf("Kelas      :X-RPL-1\n");
+printf("Umur       :15\n");
+printf("Hobi       :Membaca\n");
+return 0;
+ }
