cf7b8dd1153d8829b013c00172607679838ccd91
Nashwa Ayesha <109999606+Nashwa17@users.noreply.github.com>
2022-08-06 18:17:58 +0700

    Add files via upload

---
+++ riani biodata siswa 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA Siswa\n\n");
+printf("nama       :Riyani\n");
+printf("kelas      :X-RPL1\n");
+printf("absen      :30\n");
+printf("umur       :15\n");
+printf("hobby      :Nonton Anime\n");
+return 0;
+}
