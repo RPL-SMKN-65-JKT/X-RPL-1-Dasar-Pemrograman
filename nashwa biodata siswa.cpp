4c8523eee58b688240a3af75adbdd66eda79ccd6
Nashwa Ayesha <109999606+Nashwa17@users.noreply.github.com>
2022-08-06 18:02:37 +0700

    Add files via upload

---
+++ nashwa biodata siswa.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA Siswa\n\n");
+printf("nama       :Nashwa Ayesha Putri\n");
+printf("kelas      :X-RPL1\n");
+printf("absen      :22\n");
+printf("umur       :15\n");
+printf("hobby      :Nonton Anime, membaca buku, mendengarkan musik\n");
+return 0;
+}
