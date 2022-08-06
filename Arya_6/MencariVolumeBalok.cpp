c6938e41bfc3ec713dc9a770ee916803a0b4e5dd
athnf <70727135+athnf@users.noreply.github.com>
2022-08-06 10:57:55 +0700

    Add files via upload

---
+++ Arya_6/MencariVolumeBalok.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int p;
+int l;
+int t;
+int hasil;
+
+cout << "Type p: /n";
+cin >> p;
+cout << "Type l: /n";
+cin >> l;
+cout << "Type t: /n";
+cin >> t;
+hasil = p*l*t;
+cout <<"Hasil Adalah" << hasil;
+return 0;
+}
---
+++ Arya_6/ProgramBiodataSiswa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA KEREN \n\n");
+printf("Nama       :Arya Panca Putra\n");
+printf("Kelas      :X-RPL1\n");
+printf("Umur       :16\n");
+printf("Hobi       :Mendengarkan musik\n");
+return 0;
+ }
