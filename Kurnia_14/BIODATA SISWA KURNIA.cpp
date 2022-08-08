eecbaa4b24bae419768322c91f8c3c98205c145d
athnf <70727135+athnf@users.noreply.github.com>
2022-08-09 06:57:48 +0700

    Add files via upload

---
+++ Kurnia_14/BIODATA SISWA KURNIA.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA \n\n");
+printf("Nama       :Kurnia Lestari\n");
+printf("Kelas      :X-RPL1\n");
+printf("Umur       :16\n");
+printf("Hobi       :Baket\n");
+return 0;
+ }
