17576ff0e0f4ccc7804f6e48cf46631fef037dc4
athnf <70727135+athnf@users.noreply.github.com>
2022-08-09 07:03:15 +0700

    Add files via upload

---
+++ OktaviaAbel_26/Biodata.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA \n\n");
+printf("Nama       :Oktavia Abel\n");
+printf("Kelas      :X-RPL1\n");
+printf("Umur       :16\n");
+printf("Hobi       :Nyanyi\n");
+return 0;
+ }
