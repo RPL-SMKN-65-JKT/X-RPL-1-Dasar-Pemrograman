0231a58cf4ed10876ea909363cd1283ee99b7c54
SilverDazt <109999812+SilverDazt@users.noreply.github.com>
2022-08-09 08:34:58 +0700

    Add files via upload

---
+++ Dimas_8/Dimas_8_Nested.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 10;
+
+    if (i == 10)
+    {
+        //first if statement
+        if (i < 15)
+           cout<<i<<" is smaller than 15\n";
+
+        //nested - if statement
+        //will only be executed if statement above
+        //is true
+        if (i < 12)
+            cout<<i<<" is smaller than 12\n";
+        else
+            cout<<i<<" is greater than 15";
+    }
+    return 0;
+}
+
