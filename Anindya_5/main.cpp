76be2d019db7a23b5b3f054e00b4021a2d5ffc71
anindyaviviaputri <109999910+anindyaviviaputri@users.noreply.github.com>
2022-08-09 07:06:37 +0700

    Add files via upload

---
+++ Anindya_5/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+ syestem ("color a");
+  printf ("\tBiodata Siswa\n\n");
+  printf ("Nama    :Anindya Vivia Putri");
+  printf ("Kelas   :X-RPL\n");
+  printf ("Absen   :5\n");
+  printf ("Umur    :16\n");
+  printf ("Hobby   :Bulutangkis|n");
+  return 0;
+  )
