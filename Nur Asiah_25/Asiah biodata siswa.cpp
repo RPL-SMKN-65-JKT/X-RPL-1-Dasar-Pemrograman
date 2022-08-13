4d14066e8dfc93db2f025f24dfa5d33366f96861
Nashwa Ayesha <109999606+Nashwa17@users.noreply.github.com>
2022-08-13 14:17:35 +0700

    Add files via upload

---
+++ Nur Asiah_25/Asiah biodata siswa.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA Siswa\n\n");
+printf("nama       :Nur Asiah\n");
+printf("kelas      :X-RPL1\n");
+printf("absen      :25\n");
+printf("umur       :16\n");
+printf("hobby      :Baca komik\n");
+return 0;
+}
