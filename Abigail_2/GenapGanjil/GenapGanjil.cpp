e8cc5d350f6de0bc915fb4caf77df4650a22fd44
RetDaffarel <109999990+RetDaffarel@users.noreply.github.com>
2022-08-15 19:33:29 -0700

    Add files via upload

---
+++ Abigail_2/GenapGanjil/GenapGanjil.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+
+    cout <<"Tulis angka:";
+    cin >> x;
+
+
+
+    if  (x % 2 ==0) {
+         cout <<"Genap"<<endl;
+    }
+
+   else
+    cout <<"Ganjil"<<endl;
+
+
+    return 0;
+}
