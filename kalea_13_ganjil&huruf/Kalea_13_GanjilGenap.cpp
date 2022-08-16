7d111935b97358df5e90d5024d867233f8b72ffb
kaleaaa13 <109999653+kaleaaa13@users.noreply.github.com>
2022-08-16 09:57:59 +0700

    Add files via upload

---
+++ kalea_13_ganjil&huruf/Kalea_13_GanjilGenap.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int x;
+
+  cout << "masukkan bilangan \n";
+  cin >> x;
+
+  if (x % 2 == 0) {
+          cout << "genap";
+   }
+  else
+    cout << "ganjil";
+
+  return 0;
+}
