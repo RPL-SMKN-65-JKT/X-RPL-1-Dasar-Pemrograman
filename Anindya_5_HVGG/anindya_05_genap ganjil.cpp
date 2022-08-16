d4e47a8f955466729c750d0f1629cd99ab80c7e4
anindyaviviaputri <109999910+anindyaviviaputri@users.noreply.github.com>
2022-08-16 09:55:56 +0700

    Add files via upload

---
+++ Anindya_5_HVGG/anindya_05_genap ganjil.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+ int main (){
+     int X;
+     cout << "Tulis angka!";
+     cin >> X;
+if ('x'%2==0){
+    cout << "ganjil" << endl;}
+else
+    cout << "genap" << endl;
+
+return 0;
+ }
