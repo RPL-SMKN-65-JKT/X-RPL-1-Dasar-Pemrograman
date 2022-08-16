69ac51bbd8cdff6c2e3d7c24d6a07c317ab6f434
OdangzKetjeh <109999633+OdangzKetjeh@users.noreply.github.com>
2022-08-16 10:22:11 +0700

    Add files via upload

---
+++ fauzan_3_ganjilgenap&huruf/fauzan_3_ganjilgenap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int x;
+
+cout << "___________"<< endl;
+cout << "type a number"<< endl;
+cin >> x;
+cout << "___________" << endl;
+
+    if (x%2==0)
+        cout << "genap cuy";
+    else
+        cout << "ganjil cuy";
+    return 0;
+
+}
---
+++ fauzan_3_ganjilgenap&huruf/fauzan_3_hurufvokal.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char cc;
+
+    cout << "tulis a-z: " ;
+    cin >> cc;
+
+    if (cc == 'a')
+        cout << "huruf vokal\n";
+    else if (cc == 'i')
+        cout << "huruf vokal\n";
+    else if (cc == 'u')
+        cout << "huruf vokal\n";
+    else if (cc == 'e')
+        cout << "huruf vokal\n";
+    else if (cc == 'o')
+        cout << "huruf vokal\n";
+
+    else
+        cout << "bukan huruf vokal\n";
+
+    return 0;
+
+}
