4aca1c786011971cb4958a5299f5ad3b91759749
mukhttar123 <109999504+mukhttar123@users.noreply.github.com>
2022-08-16 10:03:14 +0700

    Add files via upload

---
+++ UMAR MUKHTAR_36_X-RPL 1/GanjilGenap/UmarMukhtar_36_GanjilGenap.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+
+    cin>>x;
+    if (x%2==0) {
+        cout << "genap" <<endl;
+    }   else {
+        cout << "ganjil" <<endl;
+    }
+    return 0;
+}
