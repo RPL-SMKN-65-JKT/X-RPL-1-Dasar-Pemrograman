2f93e6f4b5e407df93a838da60b342d090890ccd
rasyanouvalnugraha <109999708+rasyanouvalnugraha@users.noreply.github.com>
2022-08-16 10:34:07 +0700

    Add files via upload

---
+++ Rasya_28_Ganjil Genap/Rasya_28_BilanganGenap.cpp
@@ -0,0 +1,20 @@
+// Program Bilangan
+#include <iostream>
+using namespace std;
+
+int main (){
+int x;
+
+cout << "==================================================" << endl;
+cout << "Type X: ";
+cin >> x;
+cout << "==================================================" << endl;
+
+     if (x%2==0)
+        cout << "Genap";
+     else
+        cout << "Ganjil";
+
+return 0;
+}
+
