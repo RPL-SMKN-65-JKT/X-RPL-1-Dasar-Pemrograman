b90fd8980384681c364b64319311498fbfaa6ac4
KAL-EL25 <109999763+KAL-EL25@users.noreply.github.com>
2022-08-16 10:45:17 +0700

    Add files via upload

---
+++ Azreil_16_GenapGanjil/Azreil_16_GenapGanjil.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int x;
+
+cout << "=================" << endl;
+cout << "Type X: ";
+cin >> x;
+cout << "=================" << endl;
+
+    if (x%2==0)
+        cout << "Genap";
+    else
+        cout << "Ganjil";
+
+return 0;
+}
---
+++ Azreil_16_Huruf Vokal/Azreil_16_Huruf Vokal.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+char cc;
+
+cout << "==================" << endl;
+cout << "Type A-Z: ";
+cin >> cc;
+cout << "==================" << endl;
+
+
+    if (cc == 'a')
+        cout << "Huruf Vokal";
+    else if (cc == 'i')
+        cout << "Huruf Vokal";
+    else if (cc == 'u')
+        cout << "Huruf Vokal";
+    else if (cc == 'e')
+        cout << "Huruf Vokal";
+    else if (cc == 'o')
+        cout << "Huruf Vokal";
+
+    else
+        cout << "Bukan Huruf Vokal";
+
+    return 0;
+}
