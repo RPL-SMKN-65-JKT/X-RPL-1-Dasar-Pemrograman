8e7183ae118ee01241b33a71c26ce6e9ae2abd84
mukhttar123 <109999504+mukhttar123@users.noreply.github.com>
2022-08-16 10:04:01 +0700

    Add files via upload

---
+++ UMAR MUKHTAR_36_X-RPL 1/HurufVokal/UmarMukhtar_36_Huruf Vokal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char cc;
+    cin >> cc;
+
+        if (cc=='a' || cc=='i' || cc=='u' || cc=='e' || cc=='u'|| cc=='A' || cc=='I' || cc=='U' || cc=='E' || cc=='O');
+        {
+            cout<<"Huruf Vokal"<<endl;
+        }
+return 0;
+}
