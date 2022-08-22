8fded5ba61465ab82722b92687b0c47023477354
athnf <70727135+athnf@users.noreply.github.com>
2022-08-22 22:07:49 +0700

    Add files via upload

---
+++ GenapGanjil/fadhil_9_ganjilgenap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int X;
+
+cout<<"============================================"<<endl;
+cout<<"Angka : ";
+cin>>X;
+cout<<"============================================"<<endl;
+
+    if (X%2==0)
+        cout<<"GENAP";
+    else
+        cout<<"GANJIL";
+
+
+    return 0;
+
+}
