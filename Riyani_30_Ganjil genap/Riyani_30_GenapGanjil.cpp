4b1ff1d946489a36c420f23b40f4b22c37472ad2
Nashwa Ayesha <109999606+Nashwa17@users.noreply.github.com>
2022-08-22 10:51:04 +0700

    Add files via upload

---
+++ Riyani_30_Ganjil genap/Riyani_30_GenapGanjil.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    
+    cout <<"tulis angka";
+    cin >> x;
+    
+    
+   
+    if (x % 2 ==0)  {
+       cout <<"Genap"<<endl;
+    }
+    
+   else 
+    cout <<"Ganjil"<<endl;
+    
+    
+    return 0;
+}
+
