7686f369d08892faea5c1eba08965e4b39ff6e3f
tsbtansya <109999734+tsbtansya@users.noreply.github.com>
2022-08-23 06:22:27 +0700

    Add files via upload

---
+++ tsabita_35_latihan/tsabita_35_genap ganjil.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+
+     int x;
+
+     cout << "Tulis angka";
+     cin >> x;
+
+
+
+     if   (x % 2 ==0) {
+            cout <<"genap"<<endl;
+     }
+     else
+        cout <<"ganjil"<<endl;
+
+     return 0;
+}
