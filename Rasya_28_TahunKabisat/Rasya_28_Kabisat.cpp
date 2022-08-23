860297ba43859f890df095ab2515619cdb01ed8a
rasyanouvalnugraha <109999708+rasyanouvalnugraha@users.noreply.github.com>
2022-08-23 09:36:38 +0700

    Add files via upload

---
+++ Rasya_28_TahunKabisat/Rasya_28_Kabisat.cpp
@@ -0,0 +1,21 @@
+// Tahun Kabisat
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int tahun;
+
+cout << "Masukkan Tahun: ";
+cin >> tahun;
+
+    if (tahun%4==0)
+        cout << "Tahun Kabisat";
+
+
+    else
+        cout << "Bukan Tahun Kabisat";
+
+    return 0;
+
+}
