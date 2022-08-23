9c229526fb0bdb4e9832415f979aec2d74583dd7
athnf <70727135+athnf@users.noreply.github.com>
2022-08-23 11:09:34 +0700

    Add files via upload

---
+++ Fadhil_9/Maximum/fadhil_9_max.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int a;
+int b;
+
+cout<<"Angka A: \n";
+cin>> a;
+cout<<"Angka B: \n";
+cin>> b;
+
+if (a > b)
+    cout<<"Bilangan terbesar = " <<a;
+else
+    cout<<"Bilangan terbesar = " <<b;
+
+return 0;
+
+}
+
+
