13102341a5d85ff373200425b9bbc9236893035e
AryaPanca9706 <109999909+AryaPanca9706@users.noreply.github.com>
2022-08-23 10:24:50 +0700

    Add files via upload

---
+++ Arya_06_ProgramMaksimum/Arya_06_ProgramMaksimum.cpp
@@ -0,0 +1,23 @@
+// PROGRAM Maksimum
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+int a;
+int b;
+
+cout << "Input A: \n";
+cin >> a;
+cout << "Input B: \n";
+cin >> b;
+
+    if (a > b)
+        cout << "Bilangan Terbesar = " <<a;
+
+    else
+        cout << "Bilangan Terbesar = " <<b;
+
+    return 0;
+}
