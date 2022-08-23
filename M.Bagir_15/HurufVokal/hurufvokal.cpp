840e1f809c99b31c892d3880a8b76ca0dac26219
RetDaffarel <109999990+RetDaffarel@users.noreply.github.com>
2022-08-22 19:23:21 -0700

    Add files via upload

---
+++ M.Bagir_15/HurufVokal/hurufvokal.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+   char cc;
+
+   cout << "type A-Z";
+   cin >> cc;
+
+   if(cc='a')
+    cout <<"huruf vokal" <<endl;
+   else if(cc='i')
+    cout <<"huruf vokal" <<endl;
+   else if(cc='u')
+   cout <<"huruf vokal"  <<endl;
+   else if(cc='e')
+   cout <<"huruf vokal"  <<endl;
+
+   return 0;
+
+
+
+
+
+}
