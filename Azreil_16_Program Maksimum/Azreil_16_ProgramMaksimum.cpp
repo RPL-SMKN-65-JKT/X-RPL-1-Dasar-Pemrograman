57dbbafa539217f07af8c27468488ce79612f857
KAL-EL25 <109999763+KAL-EL25@users.noreply.github.com>
2022-08-23 09:14:10 +0700

    Add files via upload

---
+++ Azreil_16_Program Maksimum/Azreil_16_ProgramMaksimum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int a;
+int b;
+
+cout << "Type A: \n";
+cin >> a;
+cout << "Type B: \n";
+cin >> b;
+
+    if (a > b)
+        cout << "Bilangan Terbesar = " <<a;
+
+    else
+        cout << "Bilangan Terbesar = " <<b;
+
+    return 0;
+}
---
+++ Azreil_16_TahunKabisat/Azreil_16_TahunKabisat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int year;
+
+cout << "Type year: ";
+cin >> year;
+
+    if (year%4==0)
+        cout << "Tahun Kabisat";
+
+    else
+        cout << "Bukan Tahun Kabisat";
+
+    return 0;
+}
