657283b80028544a51d6ac1559222e7a2768e2a0
Nashwa Ayesha <109999606+Nashwa17@users.noreply.github.com>
2022-08-23 08:21:19 +0700

    Add files via upload

---
+++ Nashwa_22_kabisat/Nahwa_22_kabisat.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+
+    cout <<"tulis tahun";
+    cin >> x;
+
+
+
+    if (x % 4 ==0)  {
+       cout <<"Tahun kabisat"<<endl;
+    }
+
+   else
+    cout <<"Bukan tahun kabisat"<<endl;
+
+
+    return 0;
+}
+
