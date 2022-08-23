18cd84c2a80e1bab44acdd0329807b1c18a23f27
SilverDazt <109999812+SilverDazt@users.noreply.github.com>
2022-08-23 08:35:18 +0700

    Add files via upload

---
+++ Dimas_8/Switch/Dimas_8_Switch.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int Switch()
+{
+    char grade;
+
+    cout << "Inputkan grade (A-F): ";
+    cin >> grade;
+
+    switch (toupper(grade)){
+        case 'A':
+            cout << "Ga Nyontek nih?" << endl;
+            break;
+        case 'B':
+        case 'C':
+            cout << "Bagus!" << endl;
+            break;
+        case 'D':
+            cout << "Ngepas!"<< endl;
+            break;
+        case 'E':
+        case 'F':
+            cout << "Anda bodoh!" << endl;
+            break;
+        default:
+            cout << "Grade Salah" << endl;
+    }
+    return 0;
+}
+
