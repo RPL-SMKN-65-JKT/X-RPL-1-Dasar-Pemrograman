eb44ad0cfbefd42d5206174392488943943f412b
anindyaviviaputri <109999910+anindyaviviaputri@users.noreply.github.com>
2022-08-23 08:52:42 +0700

    Add files via upload

---
+++ Anindya_5_MTK/Anindya_05_tahun kabisatt.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int tahun;
+
+    cout <<"tahun kabisat";
+    cin>> tahun;
+if(tahun% 4==0){
+    cout<<"tahun kabisat"<<endl;}
+else{
+    cout<<"bukan tahun kabisat"<<endl;}
+return 0;
+}
+
+
