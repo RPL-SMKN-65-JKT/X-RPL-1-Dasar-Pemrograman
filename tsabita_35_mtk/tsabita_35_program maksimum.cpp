1733d0985efef3cb5ad653197118f0ade06e8b6d
tsbtansya <109999734+tsbtansya@users.noreply.github.com>
2022-08-23 08:39:26 +0700

    Add files via upload

---
+++ tsabita_35_mtk/tsabita_35_program maksimum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A,B;
+
+    cout <<"Tulis bilangan terbesar:";
+    cin >> A,B;
+
+
+    if  (A>B) {
+       cout <<"A"<<endl;
+    }
+
+   else
+    cout <<"B"<<endl;
+
+
+    return 0;
+}
