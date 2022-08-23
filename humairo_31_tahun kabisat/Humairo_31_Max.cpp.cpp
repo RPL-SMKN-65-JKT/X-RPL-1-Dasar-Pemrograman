7c8f8b37ae050c074be2c97cb2bb5613d8f01a39
humairoazzra <109999789+humairoazzra@users.noreply.github.com>
2022-08-23 08:59:55 +0700

    Add files via upload

---
+++ humairo_31_tahun kabisat/Humairo_31_Max.cpp.cpp
@@ -0,0 +1,10 @@
+Program Maksimum
+Kamus
+      A,B:Integer
+Algoritma
+     (A,B)
+     if A>B then
+        Write ('Bilangan Terbesar=',A)
+     else
+        Write ('Bilangan Terbesar=',B)
+     end if
---
+++ humairo_31_tahun kabisat/humairo_31_Kabisat.cpp.cpp
@@ -0,0 +1,10 @@
+program Tahun kabisat
+Kamus
+     Tahun:Integer
+Algoritma
+     read(tahun)
+     4 tahun mod 4=0 then
+       Write ('Tahun Kabisat')
+     else
+       Write ('Bukan tahun kabisat')
+     end if
