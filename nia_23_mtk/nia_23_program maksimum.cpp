3fe9a6af6692f77a0512be99d803303af77aed7b
NiaRahmadani <109999747+NiaRahmadani@users.noreply.github.com>
2022-08-23 08:59:54 +0700

    Add files via upload

---
+++ nia_23_mtk/nia_23_program maksimum.cpp
@@ -0,0 +1,19 @@
+#inclued <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a,b;
+
+  cout<<"Tulis Angka A:";
+  cin>>a;
+  cout<<"Tulis Angka B:";
+  cin>>b;
+
+  if(a>b)
+  {
+      cout<<"Bilangan Terbesar"<< a << endl;
+  }
+  else
+}
