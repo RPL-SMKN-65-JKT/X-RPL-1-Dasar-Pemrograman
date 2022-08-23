4e400a91dbc3f0a00186b6ef7f5e7b2371c0bffd
Nashwa Ayesha <109999606+Nashwa17@users.noreply.github.com>
2022-08-23 09:07:04 +0700

    Add files via upload

---
+++ Abel_26_max/Oktafia Abel_26_max.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int A;
+    int B;
+
+    cout << "Type A" <<endl;
+    cin >> A;
+    cout << "Typr B" <<endl;
+    cin >> B;
+
+
+if (A > B)
+    cout << "Bilangan Terbesar Adalah A= " <<A;
+
+else
+    cout << "Bilangan Terbesar Adalah B= " <<B;
+    return 0;
+}
