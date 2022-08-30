27fbaad5071129c9d6e7443c669c45042a7f0226
kaleaaa13 <109999653+kaleaaa13@users.noreply.github.com>
2022-08-30 09:09:32 +0700

    Add files via upload

---
+++ Kalea_13_KoversiAngkaTeks/kalea_13_KonversiAngkaTeks.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+int angka;
+
+cout <<"Ketik Angka Dari 1-4:";
+cin >> angka;
+
+    switch (angka) {
+    case 1:
+        cout << "Satu";
+        break;
+    case 2:
+        cout << "Dua";
+        break;
+    case 3:
+        cout << "Tiga";
+        break;
+    case 4:
+        cout << "Empat";
+        break;
+    default:
+        cout << "Angka Yang Dimasukan Salah";
+}
+
+return 0;
+}
+
