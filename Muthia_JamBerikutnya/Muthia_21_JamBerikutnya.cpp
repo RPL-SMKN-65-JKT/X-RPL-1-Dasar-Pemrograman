dc1a37e1caf2a4b3c515a6f975e4a5f4c8836775
Muthia Khairunnisa <109999607+Muthia-Khaii@users.noreply.github.com>
2022-09-06 07:36:41 +0700

    Add files via upload

---
+++ Muthia_JamBerikutnya/Muthia_21_JamBerikutnya.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    typedef struct {
+             int hh;
+             int mm;
+             int ss;
+        }jam;
+    jam j;
+
+
+
+system("color b");
+cout << "Jam      = ";
+cin >> j.hh;
+cout << "Menit    = ";
+cin >> j.mm;
+cout << "Detik    = ";
+cin >> j.ss;
+
+    cout << "=============================" << endl;
+    if (j.hh + 1)
+        cout << "Sekarang Jam " << j.hh << endl;
+    else
+        cout << "Sekarang Jam " << j.hh << endl;
+
+    if (j.mm + 1)
+        cout << "Sekarang Menit " << j.mm << endl;
+    else
+        cout << "Sekarang Menit " << j.mm << endl;
+
+    if (j.ss + 1)
+        cout << "Sekarang Detik " << j.ss << endl;
+    else
+        cout << "Sekarang Detik " << j.ss << endl;
+        cout << "=============================" << endl;
+
+
+int hasil;
+int jumlah;
+int tambah;
+
+    hasil = j.hh + 1;
+    jumlah = j.mm + 1;
+    tambah = j.ss + 1;
+
+
+    cout << "Jam Sekarang: " << hasil << endl;
+    cout << "Menit Sekarang: " << jumlah << endl;
+    cout << "Detik Sekarang: " << tambah << endl;
+    cout << "=============================" << endl;
+    cout << hasil << ":" << jumlah << ":" << tambah << endl;
+
+
+
+    return 0;
+}
