ee455b5770b2cb6d92d90b9c5a05d82ac640bc11
Nashwa Ayesha <109999606+Nashwa17@users.noreply.github.com>
2022-09-10 14:49:01 +0700

    Add files via upload

---
+++ Nisa_24_biodata siswa/Nisa_24BIODATAcpp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA \n\n ");
+printf("Nama       :Nisa nuraini\n");
+printf("Kelas      :X-RPL1\n");
+printf("Umur       :15\n");
+printf("Hobi       :berenang\n ");
+return 0;
+}
