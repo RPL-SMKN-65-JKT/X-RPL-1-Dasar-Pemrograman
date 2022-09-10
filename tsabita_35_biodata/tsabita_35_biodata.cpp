31f79536c942f73ec5db8ad8294028b490af4dce
tsbtansya <109999734+tsbtansya@users.noreply.github.com>
2022-09-10 22:02:06 +0700

    Add files via upload

---
+++ tsabita_35_biodata/tsabita_35_biodata.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA\n\n");
+printf("Nama  :Tsabita Nasyatatia Renata\n");
+printf("Kelas :X-RPL 1\n");
+printf("Absen :35\n");
+printf("Umur  :15\n");
+printf("Hobi  :Mendengarkan musik\n");
+return 0;
+}
