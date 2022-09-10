1f1d66e146b3e083aad68ae3a5e9fa6f31eafb8f
tsbtansya <109999734+tsbtansya@users.noreply.github.com>
2022-09-10 22:53:34 +0700

    Add files via upload

---
+++ nia_23_hurufVokal/nia_23_hurufVokal.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char cc;
+
+    cout << "Type A-Z:";
+    cin>>cc;
+
+    if (cc =='a')
+        cout << "Huruf vokal";
+    else if (cc =='i')
+        cout << "Huruf vokal";
+    else if (cc =='u')
+        cout << "huruf vokal";
+    else if (cc =='e')
+        cout << "huruf vokal";
+    else if (cc =='o')
+        cout << "huruf vokal";
+    return 0;
+}
