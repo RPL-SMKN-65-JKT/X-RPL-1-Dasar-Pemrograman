3deb80f27aa825ebd04a50841646041327f7642e
tsbtansya <109999734+tsbtansya@users.noreply.github.com>
2022-09-10 22:52:59 +0700

    Add files via upload

---
+++ nia_23_biodata/nia_23_Biodata.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA\n\n");
+printf("Nama  :Nia Rahmadani\n");
+printf("Kelas :X-RPL 1\n");
+printf("Absen :23\n");
+printf("Umur  :15\n");
+printf("Hobi  :Bikin TikTok\n");
+return 0;
+}
