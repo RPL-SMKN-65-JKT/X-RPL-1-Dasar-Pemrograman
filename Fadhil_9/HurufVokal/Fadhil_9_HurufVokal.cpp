987a5dcc42c4a0d86c0e5208ebf6e88fd4977e27
athnf <70727135+athnf@users.noreply.github.com>
2022-09-15 20:18:13 +0700

    Add files via upload

---
+++ Fadhil_9/HurufVokal/Fadhil_9_HurufVokal.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char cc;
+
+    cout << "Type A-Z: ";
+    cin >> cc;
+
+    if (cc='a')
+        cout << "Huruf Vokal" << endl;
+    else if (cc='i')
+        cout << "Huruf Vokal" << endl;
+    else if (cc='u')
+        cout << "Huruf Vokal" << endl;
+    else if (cc='e')
+        cout << "Huruf Vokal" << endl;
+    else if (cc='o')
+        cout << "Huruf Vokal" << endl;
+
+    return 0;
+}
