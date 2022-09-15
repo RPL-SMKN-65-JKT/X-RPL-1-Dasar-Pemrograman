07c0799ecda1a53348711fb7c457dd7ecc7961b1
RetDaffarel <109999990+RetDaffarel@users.noreply.github.com>
2022-09-15 16:16:27 +0700

    Add files via upload

---
+++ Fathir_10/HurufVokal/Fatir_10_HurufVokal.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main () {
+    char cc;
+
+    cout << "Type A-Z: ";
+    cin>>cc;
+
+    if (cc == 'a')
+        cout << "Huruf Vokal";
+    else if (cc == 'i')
+        cout << "Huruf Vokal";
+    if (cc == 'u')
+        cout << "Huruf Vokal";
+    else if (cc == 'e')
+        cout << "Huruf Vokal";
+    if (cc == 'o')
+        cout << "Huruf Vokal";
+    return 0;
+}
