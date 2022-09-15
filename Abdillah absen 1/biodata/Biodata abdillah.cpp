13d8c6612f58a2c8c985ab51c2d586de8f5ecabe
UniHitler <110000059+UniHitler@users.noreply.github.com>
2022-09-15 12:14:55 +0700

    Add files via upload

---
+++ Abdillah absen 1/biodata/Biodata abdillah.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA\n\n");
+printf("Nama        Abdillah Syekh Abu Bakar\n");
+printf("Kelas      :X-RPL1\n");
+printf("Umur       :15\n");
+printf("Hobi       :mengaji\n");
+return 0;
+ }
