da8274e929600509de6e37b2534c10cbc795ec57
tsbtansya <109999734+tsbtansya@users.noreply.github.com>
2022-09-16 19:40:29 +0700

    Add files via upload

---
+++ Sulastri_32_Biodata/Sulastri_32_Biodata.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\tBIODATA SISWA\n\n");
+printf("Nama  :Sulastri\n");
+printf("Kelas :X-RPL 1\n");
+printf("Absen :32\n");
+printf("Umur  :18\n");
+printf("Hobi  :Bikin TikTok\n");
+return 0;
+}
