93843444d13ab97ff3f52e0c621562b12148594b
humairoazzra <109999789+humairoazzra@users.noreply.github.com>
2022-09-16 19:12:23 +0700

    Add files via upload

---
+++ humairo_31_tugas/bidatasiswa.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+ system("color a");
+ printf("\tBIODATA SISWA\n\n");
+ printf("Nama    :Siti Humairo Azzra\n");
+ printf("Kelas   :X-RPL1\n");
+ printf("Absen   :31\n");
+ printf("Umur    :15\n");
+ printf("Hobby   :Denger musik\n");
+ return 0;
+ }
