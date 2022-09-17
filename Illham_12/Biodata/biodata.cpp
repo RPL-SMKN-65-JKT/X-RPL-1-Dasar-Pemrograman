b2cb9b77fa51483e0a057f3fe560637712dfe646
UniHitler <110000059+UniHitler@users.noreply.github.com>
2022-09-17 13:10:08 +0700

    Add files via upload

---
+++ Illham_12/Biodata/biodata.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color b");
+printf("\tBIODATA SISWA\n\n");
+printf("Nama        Ilham maulana\n");
+printf("Kelas      :X-RPL1\n");
+printf("Umur       :16\n");
+printf("Hobi       :menonton anime\n");
+return 0;
+}
