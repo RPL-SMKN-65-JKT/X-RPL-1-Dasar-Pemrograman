fb6a4952c7f40ec19895c77dc558efba77bc39c6
KAL-EL25 <109999763+KAL-EL25@users.noreply.github.com>
2022-10-08 22:12:00 +0700

    Add files via upload

---
+++ Azreil_16 MENU/main.cpp
@@ -0,0 +1,465 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color a");
+printf("\t   HERE IS\n");
+printf("    LIST & MENU RUMAH BEGRERAK\n");
+
+int menu;
+int minuman;
+int hasil;
+
+cout << "=======================================" << endl;
+printf("MENU MAKANAN: \n");
+printf("1.Nasi Goreng = 15.000\n");
+printf("2.Mie Rebus = 9.000\n");
+printf("3.Mie Goreng = 9.000\n");
+printf("4.Mie Ayam + Bakso = 15.000 \n");
+printf("5.Ayam Geprek + Nasi = 22.000 \n\n");
+
+cout << "PILIH MENU MAKANAN 1-5: ";
+cin >> menu;
+
+    switch(menu) {
+    case 1:
+        cout << "Nasi Goreng = RP 15.000" << endl;
+        menu=15000;
+        break;
+    case 2:
+        cout << "Mie Rebus = RP 9.000" << endl;
+        menu=9000;
+        break;
+    case 3:
+        cout << "Mie Goreng = RP 9.000" << endl;
+        menu=9000;
+        break;
+    case 4:
+        cout << "Mie Ayam + Bakso = RP 15.000" << endl;
+        menu=15000;
+        break;
+    case 5:
+        cout << "Ayam Geprek + Nasi = RP 22.000" << endl;
+        menu=22000;
+        break;
+    default:
+        cout << "Menu Makanan Hanya Sampai 5 Saja" << endl;
+        break;
+    }
+    cout << "=======================================" << endl;
+
+    printf("MENU MINUMAN: \n");
+    printf ("1.ES TEH MANIS = Rp 5.000\n");
+    printf ("2.AQUA = Rp 3.000\n");
+    printf ("3.AIR PUTIH + ES = Rp 4.000\n");
+    printf ("4.Teh Manis Panas = Rp 3.000\n");
+    printf ("5.Fanta + Es = Rp 8.000\n");
+    printf ("6.Coca Cola + Es = Rp 8.500\n\n");
+
+    cout << "PILIH MENU MNUMAN 1-6: ";
+    cin >> minuman;
+
+        switch (minuman) {
+    case 1:
+        cout << "ES TEH MANIS = 5.000" << endl;
+        minuman=5000;
+        break;
+    case 2:
+        cout << "AQUA = 3.000" << endl;
+        minuman=3000;
+        break;
+    case 3:
+        cout << "AIR PUTIH + ES = 4.000" << endl;
+        minuman=4000;
+        break;
+    case 4:
+        cout << "TEH MANIS PANAS = 3.000" << endl;
+        minuman=3000;
+        break;
+    case 5:
+        cout << "FANTA + ES = 8.000" << endl;
+        minuman=8000;
+        break;
+    case 6:
+        cout << "COCA COLA + ES = 8.500" << endl;
+        minuman=8500;
+        break;
+    default:
+        cout << "MENU MINUMAN HANYA SAMPAI 6" << endl;
+        break;
+        }
+    cout << "=======================================" << endl;
+
+    if (1&1) {
+        system ("cls");
+        hasil = menu + minuman;
+    cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (1&2) {
+        system ("cls");
+        hasil = menu + minuman;
+    cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+     } else if (1&3) {
+        system ("cls");
+        hasil = menu + minuman;
+    cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+     } else if (1&4) {
+        system ("cls");
+        hasil = menu + minuman;
+        cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+     } else if (1&5) {
+        system ("cls");
+        hasil = menu + minuman;
+        cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+     } else if (1&6) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+
+    if (2&1) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (2&2) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (2&3) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (2&4) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (2&5) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (2&6) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+
+    if (3&1) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (3&2) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (3&3) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (3&4) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (3&5) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (3&6) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    }
+
+    if (4&1) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (4&2) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (4&3) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (4&4) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (4&5) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (4&6) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    }
+
+    if (5&1) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (5&2) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (5&3) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (5&4) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (5&5) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    } else if (5&6) {
+        system ("cls");
+        hasil = menu + minuman;
+     cout << "Total Nya Jadi Rp." << hasil << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "UNTUK TAMBAHANYA" << endl;
+    cout << "RUMAH BERGERAK JUGA MENYEDIAKAN TEMPAT UNTUK BERMAIN SKATEBOARD" << endl;
+    cout << "BERIKUT LIST HARGA PERJAM NYA UNTUK BERMAIN SKATEBOARD:" << endl;
+    cout << "1 Jam: 35.000" << endl;
+    cout << "2 Jam: 70.000" << endl;
+    cout << "3 Jam: 105.000" << endl;
+    }
+
+    return 0;
+}
+}
+}
