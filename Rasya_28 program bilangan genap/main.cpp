2e51852198e7e98aef69f43ebeafd97ad21d2765
tsbtansya <109999734+tsbtansya@users.noreply.github.com>
2023-01-20 05:07:08 +0700

    Add files via upload

---
+++ Rasya_28 program bilangan genap/main.cpp
@@ -0,0 +1,13 @@
+//Program Perulangan For Bilangan Genap
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int i;
+   for(i=0;i<=100;i=i+2){
+    cout<<i<<endl;
+
+   }
+   return 0;
+}
