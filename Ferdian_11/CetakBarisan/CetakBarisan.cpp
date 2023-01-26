9fe5f653080cc86734b7b9b3e6006b24b26ac204
RetDaffarel <109999990+RetDaffarel@users.noreply.github.com>
2023-01-26 19:32:58 +0700

    Add files via upload

---
+++ Ferdian_11/CetakBarisan/CetakBarisan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    int N;
+
+    cin >> N;
+    i=0;
+    for(i=0; i < N;i++){
+        cout << "Hello World" << endl;
+    }
+
+    return 0;
+}
