b7ed3297f2628e15a3c863dba755f6c54c947c8a
athnf <70727135+athnf@users.noreply.github.com>
2023-01-26 19:31:51 +0700

    Add files via upload

---
+++ Nazhif_7/CetakBarisan/Nazhif_7_CetakBarisan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int N;
+    cout << "nilai N: ";
+    cin >> N;
+
+        for(i=1; i < N; i++){
+            cout << i << " ";
+        }
+}
