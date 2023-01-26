9256e23dbeb182fd2a777dd51ba4ff6b6f524538
RetDaffarel <109999990+RetDaffarel@users.noreply.github.com>
2023-01-26 19:41:51 +0700

    Add files via upload

---
+++ Fathir_10/Deret/Fatir_10_Deret.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main()
+{
+ cout << "##  Program C++ Deret Angka  ##" << endl;
+ cout << "===============================" << endl;
+ cout << endl;
+ 
+ int jumlah_deret,i;
+ 
+ cout << "Jumlah deret yang diinginkan: ";
+ cin >> jumlah_deret;
+ 
+ cout << endl;
+ for (i=1;i<=jumlah_deret;i++) {
+    cout << i << " ";
+ }
+ 
+ cout << endl;
+ return 0;
+}
