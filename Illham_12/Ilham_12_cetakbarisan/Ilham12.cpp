49095e834dde7a6c596322f85e33a19884bd0c0f
UniHitler <110000059+UniHitler@users.noreply.github.com>
2023-01-26 19:31:57 +0700

    Add files via upload

---
+++ Illham_12/Ilham_12_cetakbarisan/Ilham12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    int n;
+    cout<<"pilih jumlah N: ";
+    cin>>n;
+    i=1;
+    for(i=1;i<n;i++){
+        cout<<"Asep"<<endl;
+    }
+    return 0;
+}
