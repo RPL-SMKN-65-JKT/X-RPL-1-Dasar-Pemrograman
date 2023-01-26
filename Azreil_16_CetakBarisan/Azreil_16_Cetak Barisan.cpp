332dfa0e7807e27c922921a67de4f9be6fa51af5
KAL-EL25 <109999763+KAL-EL25@users.noreply.github.com>
2023-01-26 20:18:05 +0700

    Add files via upload

---
+++ Azreil_16_CetakBarisan/Azreil_16_Cetak Barisan.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color b");
+
+    int n;
+    int i;
+
+    cout << "Pilih jumlah N: ";
+    cin >> n;
+
+        for(i = 1; i < n; i++){
+            cout << i << " ";
+        }
+
+    return 0;
+}
