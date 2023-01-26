3e99ccb172fed3836db8cb2c418346ad4b5d17fa
juandenies <109999707+juandenies@users.noreply.github.com>
2023-01-26 14:53:55 +0700

    Add files via upload

---
+++ ALDY_4_CETAK BARISAN/ALDY_4_CETAK BARISAN.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    system ("color b") ;
+
+    int i;
+    int n;
+
+    cin >> n;
+    i=0;
+    for(i=0; i<n; i++) {
+        cout<< "gudmowning sunshinee" << endl;
+    }
+}
