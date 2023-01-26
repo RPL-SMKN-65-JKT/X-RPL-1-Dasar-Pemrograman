f964db6799344d1b7dc3f94abeef43e0e76bdf20
humairoazzra <109999789+humairoazzra@users.noreply.github.com>
2023-01-26 19:07:13 +0700

    Add files via upload

---
+++ oktaviaabel_26_deret/Oktafia_26_ProgramDeret.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    int i;
+    int sum=0;
+
+    cout << "Masukan N nya" << endl;
+    cin>>N;
+    for(i=1;i<N;i++){
+        sum= sum + i;
+        cout<<sum<<endl;
+    }
+    return 0;
+}
+
