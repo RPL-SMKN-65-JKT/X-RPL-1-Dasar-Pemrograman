0934d0a984fa2da5edeae2353494e8426caa6c57
athnf <70727135+athnf@users.noreply.github.com>
2023-01-26 19:06:31 +0700

    Add files via upload

---
+++ Fadhil_9/CetakBarisan/Fadhil_9_CetakBarisan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int f;
+    cout << "nilai f: ";
+    cin >> f;
+
+        for(i=1; i<f; i++){
+            cout << i << " ";
+        }
+}
