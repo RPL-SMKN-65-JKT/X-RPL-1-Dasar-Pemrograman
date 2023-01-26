2633ae83236a0b3e4feca704ad30e0d98ffedee5
athnf <70727135+athnf@users.noreply.github.com>
2023-01-26 19:09:01 +0700

    Add files via upload

---
+++ Fadhil_9/Deret/Fadhil_9_Deret.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int f;
+    int h;
+    int sum = 0;
+
+    cout << "pilih jumlah f: ";
+    cin >> f;
+
+    for(h=1; h<f; h++){
+        sum = sum + h;
+        cout << sum << endl;
+    }
+
+
+    return 0;
+}
