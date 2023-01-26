00ef3c6a009a5c3fac9e3574f691e5510d413591
tsbtansya <109999734+tsbtansya@users.noreply.github.com>
2023-01-26 19:46:58 +0700

    Add files via upload

---
+++ tsabita_34_deret.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    system ("color a") ;
+
+    int i;
+    int n;
+    int sum = 0;
+
+    cout << "pilih i";
+    cin >> n;
+
+        for(i = 1; i < n; i++){
+            sum = sum + i;
+            cout << sum << endl;
+        }
+
+
+}
