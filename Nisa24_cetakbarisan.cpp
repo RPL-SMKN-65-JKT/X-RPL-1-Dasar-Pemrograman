bb719c422da5c3a98f66343c71dbebdb03b40d5a
123mraffiA <110221861+123mraffiA@users.noreply.github.com>
2023-01-26 19:44:32 +0700

    Add files via upload

---
+++ Nisa24_cetakbarisan.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    int n;
+    cin>>n; 
+    i=1; 
+    for(i=1;i<n;i++){
+               cout<<"helloworld"<<endl; 
+     }   
+ }
---
+++ putri_27 genap.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Hello, Dcoder!";
+}
