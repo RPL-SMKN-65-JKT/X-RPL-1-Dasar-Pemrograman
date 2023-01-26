049abaa0150fcaf6d951801a7723298b1bf9517b
Nashwa Ayesha <109999606+Nashwa17@users.noreply.github.com>
2023-01-26 19:33:32 +0700

    Add files via upload

---
+++ Nashwa_22_Cetak Barisan/Nashwa_22_Cetak barisan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int i;
+    int n;
+    cin>>n;
+    i=1;
+    for(i=1;i<=n;i++){
+        cout<< "Beelze My Beloved" << endl;
+    }
+return 0;
+
+}
