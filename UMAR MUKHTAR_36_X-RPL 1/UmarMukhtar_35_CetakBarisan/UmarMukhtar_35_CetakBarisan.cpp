64255aa9f110e9c2800e73251820cd3549c427cd
mukhttar123 <109999504+mukhttar123@users.noreply.github.com>
2023-01-26 14:50:13 +0700

    Add files via upload

---
+++ UMAR MUKHTAR_36_X-RPL 1/UmarMukhtar_35_CetakBarisan/UmarMukhtar_35_CetakBarisan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int N,i;
+
+ cin >> N;
+
+ cout << endl;
+ for (i=1;i<=N;i++) {
+    cout << i << " ";
+ }
+
+ cout << endl;
+ return 0;
+}
