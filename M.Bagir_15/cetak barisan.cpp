c332d9ea3a3e35a0c81015bb4ccd008352a2b772
albagir151 <110229448+albagir151@users.noreply.github.com>
2023-01-26 17:24:11 +0700

    albagir_15_cetak barisan

---
+++ M.Bagir_15/cetak barisan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    system ("color b") ;
+
+    int i;
+    int n;
+
+    cin >> n;
+    i=0;
+    for(i=0; i<n; i++) {
+        cout<< "3 periode" << endl;
+    }
+}
+
