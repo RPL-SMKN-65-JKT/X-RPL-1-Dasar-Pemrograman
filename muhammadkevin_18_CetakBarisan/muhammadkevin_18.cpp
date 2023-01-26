a8fdad624a5adf280c396552921f4d6b90250561
tsbtansya <109999734+tsbtansya@users.noreply.github.com>
2023-01-26 14:23:03 +0700

    Add files via upload

---
+++ muhammadkevin_18_CetakBarisan/muhammadkevin_18.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main(){
+    int i;
+    int n;
+    cin>>n; 
+    i=1; 
+    for(i=1;i<n;i++){
+               cout<<"helloworld"<<endl; 
+     }   
+ }
