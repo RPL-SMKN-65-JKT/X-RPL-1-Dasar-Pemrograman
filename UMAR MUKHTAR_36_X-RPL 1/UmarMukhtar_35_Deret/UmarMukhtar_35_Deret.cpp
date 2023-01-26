bed3a0dea0890f297b11790d7fd0201c7692b13e
mukhttar123 <109999504+mukhttar123@users.noreply.github.com>
2023-01-26 15:03:51 +0700

    Add files via upload

---
+++ UMAR MUKHTAR_36_X-RPL 1/UmarMukhtar_35_Deret/UmarMukhtar_35_Deret.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int N,i;
+ int Sum=0;
+
+ cin >> N;
+ for (i=1;i<N;i++) {
+ Sum = Sum + i ;
+    cout << Sum << endl;
+ }
+
+ return 0;
+}
