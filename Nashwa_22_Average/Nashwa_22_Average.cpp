62554470e1c3635e09145b1226e620863814822a
Nashwa Ayesha <109999606+Nashwa17@users.noreply.github.com>
2023-02-02 19:41:12 +0700

    Add files via upload

---
+++ Nashwa_22_Average/Nashwa_22_Average.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int N;
+    int x;
+    int i;
+    int sum;
+    float avere;
+    cout<< "masukan N"<<endl;
+    cin >>N;
+    sum=0;
+    i=1;
+    while (i<=N){
+            cout <<" masukan angka" << endl;
+            cin >>x;
+            i++;
+            sum=sum+x;
+
+    }
+    avere = sum/N;
+    cout << "rata-rata nya adalah ="<< avere <<endl;
+}
