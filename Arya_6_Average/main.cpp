cd146a1b70966b10f70ea32fb1ad3773bca0aa3e
AryaPanca9706 <109999909+AryaPanca9706@users.noreply.github.com>
2023-02-02 15:13:11 +0700

    Add files via upload

---
+++ Arya_6_Average/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    int x;
+    int i;
+    int sum;
+    float avere;
+    cout<< "masukan N"<<endl;
+    cin >>N;
+    sum = 0;
+    i=1;
+    while(i<=N){s
+        cout<<"masukan angka"<<endl;
+        cin >>x;
+        i++;
+        sum=sum+x;
+
+    }
+    avere = sum/N;
+    cout<<"rata rata nya adalah ="<<avere<<endl;
+
+}
