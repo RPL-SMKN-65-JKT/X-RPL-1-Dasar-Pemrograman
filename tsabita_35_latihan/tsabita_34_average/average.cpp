789da629d88f9b77be453caa773d4d6ba99eee13
tsbtansya <109999734+tsbtansya@users.noreply.github.com>
2023-02-02 20:33:14 +0700

    Add files via upload

---
+++ tsabita_35_latihan/tsabita_34_average/average.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int i=1;
+    int n,x;
+    int sum =0;
+    float av;
+
+        cout<< "ketik n: ";
+        cin >> n;
+        cout<< "ketik x: ";
+        cin >> x;
+
+    while (i<=n){
+        sum=sum+x;
+        i=i+1;
+        cout<< x <<endl;
+    }
+        (i>n);
+        av=sum/n;
+        cout<< av <<endl;
+    }
+
