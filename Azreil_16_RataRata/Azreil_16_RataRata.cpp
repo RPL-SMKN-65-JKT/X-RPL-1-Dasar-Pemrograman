afb88daa3c62907cf37844e63f89913845a56e95
KAL-EL25 <109999763+KAL-EL25@users.noreply.github.com>
2023-02-02 14:58:24 +0700

    Add files via upload

---
+++ Azreil_16_RataRata/Azreil_16_RataRata.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("color b");
+
+    int i = 1;
+    int n,x;
+    int sum = 0;
+    float av;
+
+    cout << "Ketik N: ";
+    cin >> n;
+    cout << "Ketik X: ";
+    cin >> x;
+
+        while(i <= n){
+            sum = sum + x;
+            i = i + 1;
+            cout << x << endl;
+        }
+    (i > n);
+    av = sum/n;
+    cout << av << endl;
+
+
+}
