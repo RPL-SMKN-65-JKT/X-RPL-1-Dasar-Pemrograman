6980d9e7d743989f33a5dd0c6e155ff1aa64a962
humairoazzra <109999789+humairoazzra@users.noreply.github.com>
2023-02-03 05:57:55 +0700

    Add files via upload

---
+++ humairoazzra_31_average/average.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int i = 1;
+    int n,x;
+    int sum = 0;
+    float av;
+
+    cout << " N: ";
+    cin >> n;
+    cout << " X: ";
+    cin >> x;
+
+        while(i <= n){
+            sum = sum + x;
+            i = i + 1;
+            cout << x << endl;
+        }
+    (i > n);
+    av = sum/n;
+    cout << av << endl;
+
+
+}
