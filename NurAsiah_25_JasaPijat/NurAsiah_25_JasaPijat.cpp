d274bb67ab7d7322a71bab022cf34195223abcc2
anindyaviviaputri <109999910+anindyaviviaputri@users.noreply.github.com>
2023-02-13 20:56:27 +0700

    Add files via upload

---
+++ NurAsiah_25_JasaPijat/NurAsiah_25_JasaPijat.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+ using namespace std;
+
+ int main() {
+
+         int orang,i=1,sum=0,per=20000;
+         string Nama;
+         float durasi,jam=1000,j=0,total;
+
+         cout<<"\***JAPI***"<<endl;
+         cout<<"\**Jasa Pijat **"<<endl;
+         cout<<"========================================"<<endl;
+         cout<<endl;
+         cout<<"\t Jumlah orang: ";
+         cin>> orang;
+
+         for (int i=1;i<=orang;i++) {
+             cout<<"-> Orang ke:"<<i<<".";
+             cin>> Nama;
+             cout<<"   Durasi berapa jam: ";
+             cin>> durasi;
+             sum=orang*per;
+             j=j+durasi;
+         }
+         cout<<endl;
+         cout<<"     Total durasi: "<<j<<""<<endl;
+         j=j*jam;
+         total=sum+j;
+         cout<<"     Total harga :Rp. "<<total<<""<<endl;
+         cout<<"\t      terima kasih";
+
+         return 0;
+ }
