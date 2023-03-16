5dcf59c776ac384a4d5ba8571252ab1b2782fc75
RetDaffarel <109999990+RetDaffarel@users.noreply.github.com>
2023-03-16 08:41:25 +0700

    Add files via upload

---
+++ Fathir_10/Array1/Array 1_Fatir_10 (1).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int nilai[6]={72,74,70,82,93,95};
+    int i;
+    for (i=0;i<6;i++){
+        cout <<"Tampil index ke-"<<i+1<<" = "<<nilai[i]<<endl;
+    }
+    return 0;
+}
