83d7a03e612ff28c7572b8ef5bbc4114bf451b28
RetDaffarel <109999990+RetDaffarel@users.noreply.github.com>
2023-03-16 14:10:19 +0700

    Add files via upload

---
+++ Maulidia_21/Array1/MAULIDIA_15_Array1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+     
+     int ARnilai[5]={60,70,80,90,99};
+
+     for(int i=0; i<5;i++){
+         cout<<"Nilai ke ["<<i<<"] = "<<ARnilai[i];
+         cout<<endl;
+     }
+
+     return 0;
+}
