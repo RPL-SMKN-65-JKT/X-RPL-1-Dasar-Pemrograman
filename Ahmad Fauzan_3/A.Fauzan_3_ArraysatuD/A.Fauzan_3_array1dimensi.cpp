a3330520055d99d0978d31af96699063a77dfaf1
OdangzKetjeh <109999633+OdangzKetjeh@users.noreply.github.com>
2023-03-16 12:23:25 +0700

    Add files via upload

---
+++ Ahmad Fauzan_3/A.Fauzan_3_ArraysatuD/A.Fauzan_3_array1dimensi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){//program utama
+
+    int ojj[] = {60, 40, 45, 30, 50};//inisialisasi/typedata,array,index berisi value
+    cout<<"Indexnya adalah = "<<ojj[0]<<endl;//statement
+
+    for(int i=0; i<5;i++){ //inisialisasi,kondisi,increment
+          cout<<"Tampil nilai indek ke "<<i<<" = "<<ojj[i];//statement
+          cout<<endl;//statement
+    }
+}
