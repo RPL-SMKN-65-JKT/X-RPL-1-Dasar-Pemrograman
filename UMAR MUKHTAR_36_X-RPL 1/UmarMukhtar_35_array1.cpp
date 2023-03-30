21f7d1053ab5813c2e1cf62c0295a97875f94ebf
mukhttar123 <109999504+mukhttar123@users.noreply.github.com>
2023-03-30 12:14:48 +0700

    Update UmarMukhtar_35_array1.cpp

--- UMAR MUKHTAR_36_X-RPL 1/UmarMukhtar_35_array1.cpp
+++ UMAR MUKHTAR_36_X-RPL 1/UmarMukhtar_35_array1.cpp
@@ -2,12 +2,13 @@
 
 using namespace std;
 
-int main(){
-    int jml_arr;
-    string nama[] = { "Budi","Asep","Koh Aheng","Sakmadik","Marimas","Jokowi","Megawati" };
+int main(){ //fungsi utama dari suatu program
+    int jml_arr; //inisialisasi
+    string nama[] = { "Budi","Asep","Koh Aheng","Sakmadik","Marimas","Jokowi","Megawati" }; //array string berisikan nama-nama
     jml_arr = sizeof(nama)/sizeof(*nama);
 
-    for(int a=0; a< jml_arr; a++){
-        cout << nama[a]<<endl;
+    for(int a=0; a< jml_arr; a++){ //inisialisasi,kondisi,dan increment
+        cout << nama[a]<<endl; // mengeluarkan hasil output text
+      
     }
 }
