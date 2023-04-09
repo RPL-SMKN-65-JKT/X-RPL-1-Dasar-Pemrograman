1cc8811d2d074c7ab64acde526a88f5e4689a684
RetDaffarel <109999990+RetDaffarel@users.noreply.github.com>
2023-04-09 20:23:21 +0700

    Add files via upload

---
+++ Ferdian_11/RerataArray1/Ferdian_11_RerataArray1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()                                                      //fungsi utama dari suatu program
+{
+    system ("color B0");                                        //mengeluarkan suatu komponen
+  int arr[100],n;                                               //"arr[100]"digunakan untuk batas penyimpanan tersebut
+  float average, jumlah = 0;                                    //float digunakaan untuk menampilkan bilangan pecahan
+
+  cout << "Element Array: ";                                    //"cout" digunakan untuk output text
+  cin >> n;                                                     //"cin" digunakan untuk menginput text
+
+  cout <<"("<<endl;
+
+  for (int i = 0; i < n; i++){                                  //inisialisasi,kondisi,increment
+    cout << "Nilai Ke " << i+1 << " = ";                        //"i+1" digunakan untuk menghadirkan data ke berapa
+    cin >> arr[i];                                              //"arr[i]" digunakan untuk menyimpan data ke "i"
+    jumlah = jumlah + arr[i];                                   //"jumlah" digunakan untuk hasil dari semua data pada array
+  }
+  cout <<")"<<endl;
+  average = jumlah/n;
+
+  cout << "Rata-Ratanya Adalah : " << average << endl;
+}
