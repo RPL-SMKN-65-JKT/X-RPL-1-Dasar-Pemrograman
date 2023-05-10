d92c0a401c32001b8ae17cd302ec754dcdb44bcd
UniHitler <110000059+UniHitler@users.noreply.github.com>
2023-05-10 11:21:46 +0700

    Add files via upload

---
+++ Kelompok 8 abel kalea litha/p.cpp
@@ -0,0 +1,18 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int a=2;
+int b=3;
+
+void penjumlahan()
+{
+    int hasil=a+b;
+    cout << hasil;
+}
+
+int main() {
+    penjumlahan();
+    getch();
+}
