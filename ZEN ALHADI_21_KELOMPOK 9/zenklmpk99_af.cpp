0f8310166684839a71f3dcc968fa64e205677ac4
albagir151 <110229448+albagir151@users.noreply.github.com>
2023-05-10 22:14:56 +0700

    Add files via upload

---
+++ ZEN ALHADI_21_KELOMPOK 9/zenklmpk99_af.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void hitungLuasSegitiga() {
+  double alas = 5;
+  double tinggi = 7;
+  double luas = (alas * tinggi) / 2;
+  cout << "Luas segitiga adalah: " << luas << endl;
+}
+
+int main()
+{
+  hitungLuasSegitiga();
+
+  return 0;
+}
