fc19680d84e4fae39fb02e6e2ce55b0004c90865
UniHitler <110000059+UniHitler@users.noreply.github.com>
2023-05-11 08:25:34 +0700

    Add files via upload

---
+++ Kelompok 8 abel lea litha/fun.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void siswi(){
+    string nama = "Aleta";
+    string jenis_kelamin = "Perempuan";
+    cout << nama << " adalah seseorang " <<jenis_kelamin<<endl;
+}
+//fungsi= => Memiliki nilai kembalian
+string siswa(){
+    string nama = "Mahendra";
+    string jenis_kelamin = "Laki laki";
+    cout << nama << " adalah seorang " <<jenis_kelamin<<endl;
+}
+
+int main(){
+    siswi();
+    siswa();
+
+return 0;
+}
+
+
---
+++ Kelompok 8 abel lea litha/pro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Procedure tidak memiliki nilai kembalian
+void siswa(){
+    string nama = "Mahendra";
+    cout << nama <<endl;
+}
+int main(){
+    siswa();
+return 0;
+}
+
+
